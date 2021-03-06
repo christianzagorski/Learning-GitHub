e347f25cdbac44f8076149affcc611cf0f9eaba9
Christian Zagorski <christianz@hey.com>
2021-03-05 20:56:37 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Christian Zagorski on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
